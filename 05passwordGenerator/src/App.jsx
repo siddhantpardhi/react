8f4c94892a262774523a1c37510bf3db8617d37f
siddhantpardhi <siddhantpardhi10@gmail.com>
2025-05-22 13:15:27 +0530

    before react router

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 10);
+    passwordRef.current?.setSelectionRange(0, length);
     window.navigator.clipboard.writeText(password)
   }, [password])
 
--- customReact/customReact.js
+++ customReact/customReact.js
@@ -16,7 +16,9 @@ function customRender(reactElement, mainContainer) {
 
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
+
     for (const prop in reactElement.props) {
+        if(prop === "children") continue;
         domElement.setAttribute(prop, reactElement.props[prop])
     }
 
